2fa7932b421c919d545331575b52c42e02aff6fd
Farimah Fattahi <farimah.fattahi2000@gmail.com>
2024-04-17 15:55:47 +0330

    Extract HomeHeroSection component

---
+++ src/app/_components/home-hero-section/home-hero-section.test.tsx
@@ -0,0 +1,44 @@
+import { render } from "@testing-library/react";
+import { HomeHeroSection } from "./home-hero-section";
+
+describe("Home hero section", () => {
+  test("display the home hero section", () => {
+    const { container } = render(<HomeHeroSection />);
+    expect(container)
+      .toMatchSnapshot(`<section className="bg-hero-pattern bg-no-repeat bg-center xl:bg-left mt-5 xl:mt-20">
+      <div className="container flex flex-col-reverse xl:flex-row">
+        <div className="flex flex-col gap-y-8 text-center xl:text-right mt-12 pb-10">
+          <h3 className="dark:text-info text-xl xl:text-2xl">
+            خوش اومدی به ...
+          </h3>
+          <h1 className="text-xl xl:text-5xl font-black gradient">
+            مسیر صعود به قله های برنامه نویسی
+          </h1>
+          <p className="text-lg font-bold leading-8">
+            هر جای مسیر برنامه‌نویسی که باشی، با همراهی استادهای با تجربه کلاسبن
+            می‌تونی بدون محدودیت به قله‌های بالاتر صعود کنی. ما همیشه هواتو
+            داریم.
+          </p>
+          <div className="flex gap-x-4">
+            <Button variant="primary">دوره‌های ری‌اکت و نکست</Button>
+            <Button variant="neutral">مشاوره برنامه‌نویسی</Button>
+          </div>
+
+          <Image
+            src="/images/frameworks.png"
+            width={412}
+            height={39}
+            alt=""
+            className="grayscale opacity-70 m-auto xl:m-0 mt-8 xl:mt-5"
+          />
+        </div>
+        <Image
+          src="/images/programmer-landing.svg"
+          width={702}
+          height={521}
+          alt="کلاسبن"
+        />
+      </div>
+    </section>`);
+  });
+});
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,5 @@
-import Image from "next/image";
-import { Button } from "./_components/button";
+import { HomeHeroSection } from "./_components/home-hero-section/home-hero-section";
 
 export default function Home() {
-  return (
-    <section className="bg-hero-pattern bg-no-repeat bg-center xl:bg-left mt-5 xl:mt-20">
-      <div className="container flex flex-col-reverse xl:flex-row">
-        <div className="flex flex-col gap-y-8 text-center xl:text-right mt-12 pb-10">
-          <h3 className="dark:text-info text-xl xl:text-2xl">
-            خوش اومدی به ...
-          </h3>
-          <h1 className="text-xl xl:text-5xl font-black gradient">
-            مسیر صعود به قله های برنامه نویسی
-          </h1>
-          <p className="text-lg font-bold leading-8">
-            هر جای مسیر برنامه‌نویسی که باشی، با همراهی استادهای با تجربه کلاسبن
-            می‌تونی بدون محدودیت به قله‌های بالاتر صعود کنی. ما همیشه هواتو
-            داریم.
-          </p>
-          <div className="flex gap-x-4">
-            <Button variant="primary">دوره‌های ری‌اکت و نکست</Button>
-            <Button variant="neutral">مشاوره برنامه‌نویسی</Button>
-          </div>
-
-          <Image
-            src="/images/frameworks.png"
-            width={412}
-            height={39}
-            alt=""
-            className="grayscale opacity-70 m-auto xl:m-0 mt-8 xl:mt-5"
-          />
-        </div>
-        <Image
-          src="/images/programmer-landing.svg"
-          width={702}
-          height={521}
-          alt="کلاسبن"
-        />
-      </div>
-    </section>
-  );
+  return <HomeHeroSection />;
 }
