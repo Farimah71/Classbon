0c595a89b7b76be9025c084518ae7c619dba5fa9
Farimah Fattahi <farimah.fattahi2000@gmail.com>
2024-04-21 21:04:13 +0330

    Add Text placeholder component

--- src/app/(courses)/courses/[slug]/_components/comments/course-comments.tsx
+++ src/app/(courses)/courses/[slug]/_components/comments/course-comments.tsx
@@ -3,10 +3,11 @@
 import { useParams } from "next/navigation";
 import { useCourseComments } from "../../_apis/get-comments";
 import { Comment } from "@/app/_components/comment";
+import { TextPlaceholder } from "@/app/_components/placeholders";
 
 export const CourseComments = () => {
   const { slug } = useParams();
-  const { data: comments } = useCourseComments({
+  const { data: comments, isLoading } = useCourseComments({
     params: {
       slug: slug as string,
       page: 1,
@@ -17,6 +18,7 @@ export const CourseComments = () => {
       {comments?.data.map((comment) => (
         <Comment key={`comment-${comment.id}`} variant="info" {...comment} />
       ))}
+      {isLoading && <TextPlaceholder />}
     </>
   );
 };
