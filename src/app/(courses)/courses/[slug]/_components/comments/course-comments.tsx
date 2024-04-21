34989b646c3dbffa1fbb00f439fc0cf797312dfa
Farimah Fattahi <farimah.fattahi2000@gmail.com>
2024-04-21 20:20:17 +0330

    Replace React-query with Tanstack-query

--- src/app/(courses)/courses/[slug]/_components/comments/course-comments.tsx
+++ src/app/(courses)/courses/[slug]/_components/comments/course-comments.tsx
@@ -1,11 +1,23 @@
 "use client";
 
-import { readData } from "@/core/http-service/http-service";
-import { useEffect } from "react";
+import { useParams } from "next/navigation";
+import { useCourseComments } from "../../_apis/get-comments";
 
 export const CourseComments = () => {
-  useEffect(() => {
-    // readData("/validation-error");
-  }, []);
-  return <></>;
+  const { slug } = useParams();
+  const { data: comments } = useCourseComments({
+    params: {
+      slug: slug as string,
+      page: 1,
+    },
+  });
+  return (
+    <>
+      {comments?.data.map((c) => (
+        <p key={`comment-${c.id}`} className="mt-10">
+          {c.commentText}
+        </p>
+      ))}
+    </>
+  );
 };
